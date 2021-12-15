c3ffd5e90e99ae121b89b83e2b30dd1737e9cad1
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2021-12-15 19:42:56 +0000

    Updated by ALF automation.

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -3,4 +3,4 @@
 package require mysqltcl
 
 set dashircbot_poollist_subversion "1.8"
-set dashircbot_poollist_script [file tail [ dict get 
+set dashircbot_poollist_script [file tail [ dict get [ info fr
