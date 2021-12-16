51050d5b0325c1a0f3b5f9dcc3c42813c383c23d
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2021-12-16 15:14:40 +0000

    Add ToDo

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -3,4 +3,4 @@
 package require mysqltcl
 
 set dashircbot_poollist_subversion "1.8"
-set dashircbot_poollist_script [file tail [ dict get [ info frame [ info frame ] ]
+set dashircbot_poollist_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
