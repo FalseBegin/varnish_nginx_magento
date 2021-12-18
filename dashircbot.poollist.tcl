2e6229c94d6e459c2e6c428b52057e12869d8006
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2021-12-18 14:22:09 +0000

    Update travis matrix

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -5,4 +5,4 @@ package require mysqltcl
 set dashircbot_poollist_subversion "1.8"
 set dashircbot_poollist_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
 
-putlog "++ $::dashircbot_pooll
+putlog "++ $::dashircbot_poollist_script v$
