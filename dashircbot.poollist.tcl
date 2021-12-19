87fc270a2eb172a44e73e72cfabda8413db14a32
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2021-12-19 18:45:01 +0000

    Added line break

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -5,4 +5,4 @@ package require mysqltcl
 set dashircbot_poollist_subversion "1.8"
 set dashircbot_poollist_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
 
-putlog "++ $::dashircbot_poollist_script v$dashircbot_poollist_sub
+putlog "++ $::dashircbot_poollist_script v$dashircbot_poollist_subversion lo
