11d4f0db0895f3799aa3f58aed0c987db1a2e49b
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2021-12-20 07:27:58 +0000

    small layout fix

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -5,4 +5,6 @@ package require mysqltcl
 set dashircbot_poollist_subversion "1.8"
 set dashircbot_poollist_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
 
-putlog "++ $::dashircbot_poollist_script v$dashircbot_poollist_subversion lo
+putlog "++ $::dashircbot_poollist_script v$dashircbot_poollist_subversion loading..."
+
+proc do
