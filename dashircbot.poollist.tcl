01d25b567bb8ecd573bed6404be243b1453c20e2
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-01-02 19:23:43 +0000

    hwmon: (lm78) Fix overflow problems seen when writing large temperature limits

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -17,4 +17,4 @@ proc do_poollist_aux {header data} {
       lassign $info username url type miningfee wdmin wdfeeauto wdfeeman
       set newentry "( $url \[$type/$miningfee\] Withdraw >=$wdmin fees auto=$wdfeeauto man=$wdfeeman )"
       set newentrylen [string length $newentry]
-      set t
+      set totallen 
