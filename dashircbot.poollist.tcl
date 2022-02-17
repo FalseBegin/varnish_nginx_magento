7fce35c2a01354e1d14987240884920852f4c7d5
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-02-17 13:21:58 +0000

    Minor tweak

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -48,4 +48,4 @@ proc do_poollist_aux {header data} {
 }
 
 proc do_poollist {nick chan} {
-  putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_
+  putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_script v$::d
