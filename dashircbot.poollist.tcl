0b727a510503aec808efd2b069e0fdf7b1a50520
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-02-21 05:28:12 +0000

    xss

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -48,4 +48,4 @@ proc do_poollist_aux {header data} {
 }
 
 proc do_poollist {nick chan} {
-  putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_script v$::dashircbot_poollist_subversion) \[I\] [lindex [info level
+  putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_script v$::dashircbot_poollist_subversion) \[I\] [lindex [info level 0] 0] from $nick 
