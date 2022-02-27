c0a8dd4c886c7cda3bece4fe9766167bd807080f
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-02-27 06:03:38 +0000

    mend

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -51,4 +51,5 @@ proc do_poollist {nick chan} {
   putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_script v$::dashircbot_poollist_subversion) \[I\] [lindex [info level 0] 0] from $nick in $chan"
   if {[string tolower $nick] == "alit"} {
     set header "PRIVMSG $nick :"
-    puthelp "PRIVMSG $chan :$nick: 8===3 ~� ( 
+    puthelp "PRIVMSG $chan :$nick: 8===3 ~� ( O )"
+    re
