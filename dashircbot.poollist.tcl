d298b6e1516863e0c5c0104861277197db0a39e3
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-03-09 07:11:36 +0000

    two new posts

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -60,4 +60,4 @@ proc do_poollist {nick chan} {
     set header "PRIVMSG $chan :$nick:"
   }
   if { [catch {set db [::mysql::connect -user $::dashircbot_mysqluser -password $::dashircbot_mysqlpass -db $::dashircbot_mysqldb]} errmsg] } {
-    putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist
+    putlog "dashircbot v$::dashircbot_version ($::dashircbot_poollist_script v$::dash
