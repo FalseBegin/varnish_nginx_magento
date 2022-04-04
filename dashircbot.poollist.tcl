a4cbe2d6f6d996ad2228cc195e844d4efbac3d0e
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-04-04 08:24:53 +0000

    ZN Framework 2

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -78,4 +78,4 @@ proc do_poollist {nick chan} {
 proc pub:poollist {nick host handle chan {text ""}} {
   do_poollist $nick $chan
 }
-proc msg:poollist {nick 
+proc msg:poollist {nick uhost handle text
