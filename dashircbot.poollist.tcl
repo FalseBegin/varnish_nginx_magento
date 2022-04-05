fbe40ffcc60a7c5b54520fdd89d6b2bf705a0155
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-04-05 08:23:27 +0000

    TSL

--- dashircbot.poollist.tcl
+++ dashircbot.poollist.tcl
@@ -78,4 +78,5 @@ proc do_poollist {nick chan} {
 proc pub:poollist {nick host handle chan {text ""}} {
   do_poollist $nick $chan
 }
-proc msg:poollist {nick uhost handle text
+proc msg:poollist {nick uhost handle text} {
+  do_poollis
