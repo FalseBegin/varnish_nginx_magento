1313accdf3d780ce31d0b9530e56001f0141bbc0
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-04-23 05:58:40 +0000

    ath9k: Fix softlockup in AR9485

--- dashircbot.tcl
+++ dashircbot.tcl
@@ -2,4 +2,5 @@ set dashircbot_version "1.1.0"
 
 set dashircbot_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
 
-set dashircbot_command_en
+set dashircbot_command_en ""
+set dash
