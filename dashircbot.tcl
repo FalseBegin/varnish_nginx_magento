9c4624dcbda0489222fd34f865700889340d3455
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-05-20 16:15:29 +0000

    remove debugs

--- dashircbot.tcl
+++ dashircbot.tcl
@@ -30,4 +30,4 @@ set dashircbot_command_fr [lsort -command dashircbot_command_cmp $dashircbot_com
 set idxn 0
 set totnen [llength $dashircbot_command_en]
 set totn [expr $totnen-1]
-foreach line $da
+foreach line $dashircbot_co
