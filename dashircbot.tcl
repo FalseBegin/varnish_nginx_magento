a5712765d51bbf554b0669b3c172acf7c13fc62f
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-05-21 16:54:36 +0000

    Update manifest file

--- dashircbot.tcl
+++ dashircbot.tcl
@@ -30,4 +30,5 @@ set dashircbot_command_fr [lsort -command dashircbot_command_cmp $dashircbot_com
 set idxn 0
 set totnen [llength $dashircbot_command_en]
 set totn [expr $totnen-1]
-foreach line $dashircbot_command_en 
+foreach line $dashircbot_command_en {
+  set comman
