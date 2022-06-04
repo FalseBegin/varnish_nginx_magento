868f8e83650a70e44ce65a6a0d3b888dedfb37b2
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-06-04 20:03:16 +0000

    StyleCop

--- dashircbot.tcl
+++ dashircbot.tcl
@@ -46,4 +46,5 @@ foreach line $dashircbot_command_en {
 set idxn 0
 set totnfr [llength $dashircbot_command_fr]
 set totn [expr $totnen-1]
-foreach line $dashircbot
+foreach line $dashircbot_command_fr {
+ 
