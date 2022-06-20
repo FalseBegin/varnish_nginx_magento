345627b9dc561b68f0179540fb63dbf9c5baec9f
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-06-20 13:17:12 +0000

    raw

--- dashircbot.twitter.tcl
+++ dashircbot.twitter.tcl
@@ -12,3 +12,13 @@ set dashircbot_twitter_lasttweetid 0
 putlog "++ $::dashircbot_twitter_script v$dashircbot_twitter_subversion loading..."
 
 proc do_fetch_lasttweetid {} {
+  if {$::dashircbot_twitter_lasttweetid == 0} {
+    if { [catch {set db [::mysql::connect -user $::dashircbot_mysqluser -password $::dashircbot_mysqlpass -db $::dashircbot_mysqldb]} errmsg] } {
+      putlog "dashircbot v$::dashircbot_version ($::dashircbot_twitter_script v$::dashircbot_twitter_subversion) \[E\] [lindex [info level 0] 0] $errmsg"
+    } else {
+      if { [catch {set data [::mysql::sel $db "SELECT StatValue FROM cmd_stats_values WHERE StatKey = 'tweetlastdrkc'" -list]} errmsg] } {
+        putlog "dashircbot v$::dashircbot_version ($::dashircbot_twitter_script v$::dashircbot_twitter_subversion) \[E\] [lindex [info level 0] 0] $errmsg"
+        ::mysql::close $db
+      } else {
+        ::mysql::close $db
+        set ::dashircbot_twitter_lasttweetid [lindex $data 0]
