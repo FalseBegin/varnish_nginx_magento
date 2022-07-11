64996d28838c87af0f53530263dd3aaf758a396c
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-07-11 15:11:58 +0000

    update it translation

--- dashircbot.worth.tcl
+++ dashircbot.worth.tcl
@@ -1,3 +1,11 @@
 
 #!/usr/bin/tclsh
 # Worth command for dashircbot
+package require http
+package require tls
+package require json
+
+::http::register https 443 [list ::tls::socket -tls1 1]
+
+set dashircbot_worth_subversion "2.17"
+set dashircbot_worth_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
