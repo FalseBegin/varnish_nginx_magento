6975cdab2fd3ed93adca3774498c961c0e750697
FalseBegin <99493717+FalseBegin@users.noreply.github.com>
2022-07-11 17:55:28 +0000

    Updated test suite

--- dashircbot.worth.tcl
+++ dashircbot.worth.tcl
@@ -9,3 +9,16 @@ package require json
 
 set dashircbot_worth_subversion "2.17"
 set dashircbot_worth_script [file tail [ dict get [ info frame [ info frame ] ] file ]]
+
+set dashircbot_translation [dict create \
+                                    "usage_calc" [dict create \
+        "en" "Usage: !calc\[fiat\] <hashrate_in_khs> - \[fiat\] can be eur or usd (default: usd) - Ex: !calc 10000" \
+        "fr" "Utilisation: !calc\[fiat\] <hachage_en_khs> - \[fiat\] peux être eur ou usd (par défaut: usd) - Ex: !calc 10000\]"] \
+                                    "usage_diff" [dict create \
+        "en" "Usage: !diff \[difficulty_value\] - If no difficulty is given as parameter, will use current one." \
+        "fr" "Utilisation: !diff \[valeur_difficulté\] - Si la difficulté n'est pas spécifié, utilisera l'actuelle."] \
+                                    "usage_mnworth" [dict create \
+        "en" "Usage: !mnworth\[fiat\] <number_of_masternodes> - \[fiat\] can be eur or usd (default: usd) - Ex: !mnworth 2 or !mnw" \
+        "fr" "Utilisation: !mnvaleur\[fiat\] <nombre_de_masternodes> - \[fiat\] peux être eur ou usd (par défaut: usd) - Ex: !mnvaleur 2 ou !mnv"] \
+                                    "usage_worth" [dict create \
+        "en" "Usage: !worth\[fiat\] <amount_DASH|Dash_Address> - \[fiat\] can be eur or usd (default: usd) - Ex: !worth 1234.5 or !worth Xr57hNKbEzNHFkTsUmfhPxKRfnnt9nVe7z or !w 76" \
